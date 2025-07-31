879e7614575543605867e32ada2e102eb68b48dd
roeckerdiego <diego.bram.oliveira@escola.pr.gov.br>
2025-07-31 12:17:53 +0000

    primeira parte do jogo

---
+++ jogo.py
@@ -0,0 +1,12 @@
+import random 
+print("🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶🕶")
+print("                           ")
+print("😃  Jogo de adivinhação  😃")
+print("         criado por        ")
+print("😍          diego        😍")
+print("                           ")
+print("🧐🧐🧐🧐🧐🧐🧐🧐🧐🧐🧐🧐🧐")
+
+numeroSecreto = random.randrange(0,100)
+totalTentativas = 0
+pontos = 100
