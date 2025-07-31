fffd4085a17b66ecedf7507e7016b71a1f24638c
roeckerdiego <diego.bram.oliveira@escola.pr.gov.br>
2025-07-31 14:49:50 +0000

    parte2 do jogo

--- jogo.py
+++ jogo.py
@@ -10,3 +10,21 @@ print("🧐🧐🧐🧐🧐🧐🧐🧐🧐🧐🧐🧐🧐")
 numeroSecreto = random.randrange(0,100)
 totalTentativas = 0
 pontos = 100
+
+print("                               ")
+print("Qual os níveis de dificuldade? ")
+print("(1)- Fácil (2)- Médio (3)- Difícil ")
+
+nivel = int(input("Escolha um nível: "))
+
+if (nivel == 1):
+    print("Você tem 20 tentativas. iiii ainda é um franguinho.")
+    totalTentativas = 20
+elif (nivel == 2):
+    print("Você tem 10 tentativas. iiii ainda é um galizé.")
+    totalTentativas = 10
+elif (nivel == 3):
+    print("Você tem 5 tentativas. iiii já é um galinho.")
+    totalTentativas = 5
+else:
+    print("número inválido")
