22bfba02f634e387759f744a0c9f498195aa01e8
roeckerdiego <diego.bram.oliveira@escola.pr.gov.br>
2025-08-04 13:54:26 +0000

    finalização loop

--- jogo.py
+++ jogo.py
@@ -28,3 +28,16 @@ elif (nivel == 3):
     totalTentativas = 5
 else:
     print("número inválido")
+
+for rodada in range (1, totalTentativas +1):
+    print("Tentativa {} de {}".format(rodada,totalTentativas) )
+    chute_str = input("Digite um número entre 1 a 100: ")
+    chute = int(chute_str)
+
+    if(chute < 1 or > 100):
+        print("Número inválido")
+        continue
+
+    acertou = chute == numeroSecreto
+    maior = chute > numeroSecreto
+    menor = chute < numeroSecreto
